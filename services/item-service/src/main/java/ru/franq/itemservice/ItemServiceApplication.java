e49177003da564b2dea4ae589a6e0d1cdac19e97
Roman T <good.romka@yandex.ru>
2022-07-27 14:57:16 +0300

    create item-service

---
+++ services/item-service/src/main/java/ru/franq/itemservice/ItemServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.franq.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+
+}
